a006f02051a9c6e2a5d64df79f36bfe150bbc94f
Sandro <sandrus88@hotmail.it>
2021-01-29 00:18:53 +0100

    classi java

---
+++ src/org/sg/handle/CampusXml.java
@@ -0,0 +1,24 @@
+package org.sg.handle;
+
+public class CampusXml {
+	public String getAllStudents() {
+		return "";
+	}
+	
+	public String getStudent() {
+		return "";
+	}
+	
+	public void removeStudent() {
+		
+	}
+	
+	public String getTopics() {
+		return "";
+	}
+	
+	public String getAllCourses() {
+		return "";
+	}
+	
+}
--- src/org/sg/handle/XmlDao.java
+++ src/org/sg/handle/XmlDao.java
@@ -1,5 +1,11 @@
 package org.sg.handle;
 
+import com.sun.org.apache.xerces.internal.parsers.DOMParser;
+
 public class XmlDao {
+	DOMParser parser = new DOMParser();
 	
+	public XmlDao() {
+
+	}
 }
