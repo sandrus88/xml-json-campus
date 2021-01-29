c248f8c0ab29b3ad37d89ec01ca72ae624c4aa13
Sandro <sandrus88@hotmail.it>
2021-01-29 12:03:00 +0100

    metodo getAllStudents

---
+++ src/org/sg/handle/CampusXml.java
@@ -0,0 +1,46 @@
+package org.sg.handle;
+
+import org.xml.sax.SAXNotSupportedException;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXNotRecognizedException;
+
+public class CampusXml extends XmlDao {
+	
+	public CampusXml() throws SAXNotRecognizedException, SAXNotSupportedException {
+		super();
+	}
+	
+	public String getAllStudents() throws SAXException, IOException {
+			String allStudents = "";
+			parser.parse( new InputSource(new StringReader(allStudents)) );
+			Document document=parser.getDocument();
+			Node typePacket=document.getChildNodes().item(0);
+			allStudents = typePacket.getNodeName();
+		return allStudents;
+	}
+	
+	public String getStudent() {
+		return "";
+	}
+	
+	public void removeStudent() {
+		
+	}
+	
+	public String getTopics() {
+		return "";
+	}
+	
+	public String getAllCourses() {
+		return "";
+	}
+
+
+}
