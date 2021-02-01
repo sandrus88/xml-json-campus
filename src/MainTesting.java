c3582ce4a74fc36aa25ee01d6a3222ae2a025522
Sandro <sandrus88@hotmail.it>
2021-02-01 11:55:03 +0100

    dao che legge i dto + service che testa il tutto

--- src/MainTesting.java
+++
@@ -1,6 +0,0 @@
-
-public class MainTesting {
-	public static void main(String[] args) {
-		
-	}
-}
--- src/org/sg/service/CampusService.java
+++ src/org/sg/service/CampusService.java
@@ -1,5 +1,15 @@
 package org.sg.service;
 
+import java.io.IOException;
+
+import org.sg.dao.CampusDao;
+import org.xml.sax.SAXException;
+
 public class CampusService {
-	
+	public static void main(String[] args) throws SAXException, IOException {
+		CampusDao campus = new CampusDao();
+		campus.getStudents();
+		campus.getTopics();
+		campus.getCourses();
+	}
 }
