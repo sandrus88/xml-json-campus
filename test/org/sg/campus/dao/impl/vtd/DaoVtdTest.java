0b817770696af6cbfe2c9677ca06fa7a693203d0
Ermal <ermal.aliraj@gmail.com>
2021-02-03 13:21:57 +0100

    vtd: junit for create student

--- test/org/sg/campus/dao/impl/vtd/DaoVtdTest.java
+++ test/org/sg/campus/dao/impl/vtd/DaoVtdTest.java
@@ -2,11 +2,13 @@ package org.sg.campus.dao.impl.vtd;
 
 import org.junit.jupiter.api.Test;
 import org.sg.campus.dao.StudentDao;
+import org.sg.campus.model.PaymentType;
 import org.sg.campus.model.Student;
 import org.sg.util.SGUtil;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class DaoVtdTest {
@@ -25,5 +27,21 @@ public class DaoVtdTest {
         Student student = dao.get(101);
         System.out.print(student);
         assertNotNull(student);
+    }
+    
+    @Test
+    public void test_create() throws Exception {
+        // Given
+        final int studentId = 201;
+        StudentDao dao = new StudentDaoImpl("./xml/campus_list.xml");
+        
+        //When
+        dao.create(studentId, "ermal2", "aliraj2", null);
+    
+        //Then
+        Student student = dao.get(studentId);
+        System.out.print(student);
+        assertNotNull(student);
+        assertEquals(studentId, student.getId());
     }
 }
